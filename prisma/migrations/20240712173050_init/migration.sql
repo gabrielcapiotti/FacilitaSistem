bb89ece6833d591da8e13c4989fcc20e890901b4
gabrielcapiotti <gabrielcapiottialfa@gmail.com>
2024-07-12 16:41:47 -0300

    atualizado

--- prisma/migrations/20240712173050_init/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "taskId" TEXT NOT NULL PRIMARY KEY,
-    "taskName" TEXT NOT NULL,
-    "taskDesc" TEXT NOT NULL
-);
---
+++ prisma/migrations/20240712193440_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "taskId" TEXT NOT NULL,
+    "taskName" TEXT NOT NULL,
+    "taskDesc" TEXT NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("taskId")
+);
